a1ca0531168cd20c0333fc057a891c2ed3c4af95
Padmaja <109962029+PadmajaKothapalli@users.noreply.github.com>
2022-08-12 16:22:03 +0530

    Add files via upload

---
+++ a.js
@@ -0,0 +1,10 @@
+let emp={
+    id : 19, 
+    Name : "Chandler",
+    email :"sarcasm@joey.com",
+    salary : 50000,
+    jobrole : "Data processor"
+
+}
+console.log(emp);
+console.log(typeof emp);
---
+++ c.js
@@ -0,0 +1,3 @@
+var a=1000;
+var b=89;
+console.log(a==b);
